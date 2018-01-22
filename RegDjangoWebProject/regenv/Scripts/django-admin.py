542be21cdd76b10a46717e2c07aa3e1e9533c3a6
Reginald Ledain <reginald.ledain@hmhco.com>
2018-01-22 09:27:13 -0400

    Try 2

---
+++ RegDjangoWebProject/regenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\ledainr\source\repos\RegDjangoWebProject\RegDjangoWebProject\regenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
